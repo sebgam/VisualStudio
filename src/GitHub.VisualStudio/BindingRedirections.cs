bbf18c6e05f360c51688cb0ff8eff77675ef5ddd
Jamie Cansdale <jcansdale@gmail.com>
2017-02-20 16:35:40 +0000

    Geneate .pkgdef binding redirects from code
    Use `ProvideBindingRedirection` attributes in `BindingRedirections.cs`
    give assemblies a codebase and ensure they can be found.

---
+++ src/GitHub.VisualStudio/BindingRedirections.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.Shell;
+
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "GitHub.StartPage", PublicKeyToken = "bc1bd09f2901c82e", Culture = "neutral",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = AssemblyVersionInformation.Version,
+    NewVersion = AssemblyVersionInformation.Version,
+    CodeBase = @"$PackageFolder$\GitHub.StartPage.dll")]
+
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "GitHub.UI", PublicKeyToken = "bc1bd09f2901c82e", Culture = "neutral",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = AssemblyVersionInformation.Version,
+    NewVersion = AssemblyVersionInformation.Version,
+    CodeBase = @"$PackageFolder$\GitHub.UI.dll")]
+
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "GitHub.VisualStudio", PublicKeyToken = "bc1bd09f2901c82e", Culture = "neutral",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = AssemblyVersionInformation.Version,
+    NewVersion = AssemblyVersionInformation.Version,
+    CodeBase = @"$PackageFolder$\GitHub.VisualStudio.dll")]
+
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "GitHub.Exports", PublicKeyToken = "bc1bd09f2901c82e", Culture = "neutral",
+    OldVersionLowerBound = "0.0.0.0", OldVersionUpperBound = AssemblyVersionInformation.Version,
+    NewVersion = AssemblyVersionInformation.Version,
+    CodeBase = @"$PackageFolder$\GitHub.Exports.dll")]
